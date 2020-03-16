ebafd5e8eb0cffd7d5db336c12fd0d8e1064bb7a
MRAndrade <maxreisandrade@gmail.com>
2020-03-16 19:09:24 -0300

    Finished edit functionality

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//setting variables to all HTML elements so I can target them
+//Setting variables to all HTML elements so I can target them
 
 var newItem = document.getElementById('new-item');
 var addBtn = document.getElementById('add-btn');
@@ -7,7 +7,7 @@ var todoList = document.getElementById('todo-list');
 var doneItem = document.getElementById('is-done');
 var counterList = document.getElementById('counter-list');
 
-//adding Listerers so I can add items and clear the whole list
+//Adding listener to the input and the 'add-btn', so it can add new items to the list
 
 newItem.addEventListener("keydown", function(e){
   if (e.keyCode === 13) {
@@ -18,12 +18,19 @@ newItem.addEventListener("keydown", function(e){
 addBtn.addEventListener("click", function(){
   addItem(newItem.value);
 });
+
+//Adding listener to the clear-btn so it can clear the entire list
 clearBtn.addEventListener("click", clearList);
+
 initialLoad();
 
-function addItem(text, edit, item){
+//That's the function reponsible for adding items to the screen. It has 2 argumentos: the value it's going to checked and if this function is being called through edit option instead of adding
+function addItem(text, edit){
+
+  //Variable that will hold the final value
   var todoText = "";
 
+  //Creating a loop that will go through the entire input value
   for(var i = 0; i < text.length; i++){
       
     if (
@@ -63,15 +70,15 @@ function addItem(text, edit, item){
 
   if (todoText !== "") {
 
+    //If it comes from editItem, it will return the value after being checked by the conditions above
     if(edit === true) {
-      var p = item.parentNode.getElementsByTagName('p');
-      var paragraph = p[0];
-      paragraph.innerHTML = todoText;
-      paragraph.classList.toggle('none');
-      item.classList.toggle('none');
+      return todoText;
     }
 
+    //Else, it will create a new item to the list
     else {
+      //All elements for a new item are created here: checkbox, text displayed on the item, edit option, delete option and the 'li' element that will hold it all inside itself
+
       var check = document.createElement('input');
       check.setAttribute('type','checkbox');
 
@@ -84,6 +91,7 @@ function addItem(text, edit, item){
 
       var editBtn = document.createElement('button');
       editBtn.setAttribute('class','edit-btn');
+      editBtn.append('Edit');
 
       var deleteBtn = document.createElement('button');
       deleteBtn.setAttribute('class','delete-btn');
@@ -106,31 +114,55 @@ function addItem(text, edit, item){
 }
 
 function editItem(){
+  //Variable that will hold the result from addItem condition checker
+  var value = "";
+
+  //Targeting paragraph from item
   var getParagraph = this.parentNode.getElementsByTagName('p');
   var p = getParagraph[0];
   p.classList.toggle('none');
 
+  //Targeting editBox from item
   var getEditBox = this.parentNode.getElementsByClassName('edit-box');
   var editBox = getEditBox[0];
   editBox.classList.toggle('none');
+
   editBox.focus();
+
   editBox.addEventListener("keydown", function(e){
     if (e.keyCode === 13) {
-      addItem(editBox.value, true, this);
+      //When hit enter, it will run addItem and check if the input value is valid
+      value = addItem(editBox.value, true);
+
+      //Preventing from value being set as undefined
+      if (value !== undefined) {
+        p.innerHTML = value;
+      }
+
+      //Preventing from bug that toggle double times after the first edit
+      if (p.classList.contains('none')) {
+        p.classList.toggle('none');
+      }
+      if (!editBox.classList.contains('none')) {
+        editBox.classList.toggle('none');
+      }
     };
   });
 }
 
+//Function that removes the item clicked
 function removeItem(){
   this.parentNode.remove();
   initialLoad();
 }
 
+//Function responsible for clearing the entire list
 function clearList() {
   todoList.innerHTML = "";
   initialLoad();
 }
 
+//Checking how many items are done and keeping the updated number on the screen
 function isDone(){
   var counter = 0;
   var items = document.getElementsByClassName('todo-text');
@@ -142,43 +174,44 @@ function isDone(){
   doneItem.innerHTML = counter;
 }
 
+//Checking how many items there are on the list
 function countList() {
   var counter = todoList.childElementCount;
   counterList.innerHTML = '/' + counter;
 }
 
+//Toggle between done and undone item
 function toggleDone(){
-  /*this.parentNode.classList.toggle('done'); */
   this.parentNode.querySelector('p').classList.toggle('done');
 }
 
-//This function is called when screen is first loadead, a new item is added or the list is cleared
+//This function is called when screen is first loadead, a new item is added or the list is cleared. We need to do it so it can
 function initialLoad(){
 
-  //keeping focus on new item input
+  //Keeping focus on new item input
   newItem.focus();
 
-  //keeping up the counter
+  //Keeping up the counter
   isDone();
   countList();
 
-  //creating a variable that holds all existing checkboxes at the moment
+  //Creating a variable that holds all existing checkboxes at the moment
   var checkboxItems = document.querySelectorAll('input[type="checkbox"]');
   if(checkboxItems.length === 0){
     doneItem.innerHTML = 0;
   }
 
-  //adding a listener to every existing checkbox
+  //Adding a listener to every existing checkbox
   for (var i = 0; i < checkboxItems.length; i++){
     checkboxItems[i].addEventListener('click', toggleDone);
     checkboxItems[i].addEventListener('click', isDone);
   }
 
-  //creating a variable that holds all existing delete and edit buttons from items at the moment
+  //Creating a variable that holds all existing delete and edit buttons from items at the moment
   var deleteBtns = document.querySelectorAll('button[class="delete-btn"]');
   var editBtns = document.querySelectorAll('button[class="edit-btn"]');
 
-  //adding a listener to every delete button so it can delete the item which holds it
+  //Adding a listener to every delete button so it can delete the item which holds it, the same applies for editting
   for (var i = 0; i < deleteBtns.length; i++){
     deleteBtns[i].addEventListener('click', removeItem);
     editBtns[i].addEventListener('click', editItem);
